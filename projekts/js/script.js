51424a2a46309c342a5c80808e26b2ed187ebec8
KustHosty <maxmarsharov@icloud.com>
2026-06-02 19:39:29 +0300

    Delete projekts directory

--- projekts/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', function(event) {
-    var link = event.target.closest('a');
-    if (link && link.href && !link.href.startsWith('javascript:')) {
-        event.preventDefault();
-        alert('Страница в разработке');
-    }
-});
